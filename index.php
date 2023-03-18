534ecab65bc9b0084475c1d020206a8129bc810a
Robson Barros <robsonfullstackweb@gmail.com>
2023-03-18 14:24:57 -0300

    iniciado front-end para uma base

--- index.php
+++ index.php
@@ -5,10 +5,32 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Sistema de reserva v: 0.1 </title>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital@1&display=swap" rel="stylesheet">
+
 	<link rel="stylesheet" type="text/css" href="<?php echo INCLUDE_PATH;?>/css/style.css">
 	
 </head>
 <body>
+<header>
+	<div class="context-center">
+	<div class="logotipo">
+		<h2>Robson Barros</h2>
+	</div>
+	<nav class="menu-desktop">
+		<ul>
+			<li><a href="#">Reservas</a></li>
+			<li><a href="#">OPCAO2</a></li>
+			<li><a href="#">OPCAO3</a></li>
+		</ul>
+	</nav><!--menu desktop-->
+</div><!--context-center-->
+</header>
+
+
+
+
 
 </body>
 </html>
